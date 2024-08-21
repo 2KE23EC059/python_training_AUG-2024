74be547615db21900a0c35bf366ec044dcccc715
2KE23EC059 <malleshshigihalli71@gmail.com>
2024-08-21 13:47:25 +0530

    Delete p2.py

--- p2.py
+++
@@ -1,9 +0,0 @@
-#read a number from the user anf check if it is on even number or not 
-
-#to read data from the console we can use input ().however the input()always reads only a string as usual with all languages
-
-#we must 1st cast(convert) the string into a number
-# the input(),not just reads a string but also can print  string
-my_number = int(input('enter a nuber to check if it is even or not:'))
-
-print()
